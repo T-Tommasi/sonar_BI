b1d525f39474ee8b7779f0f3a17be2e4aa86f492
T-Tommasi <ttommasi1945@gmail.com>
2025-10-16 11:11:38 +0200

    (FEAT) Added Iconify graphics
    - Added icons for clearer view
    - Added better navigations
    
    (MOD) Removed redundant and invalid component syntax
    
    - Removed repetitive and invalid content from components (sideMenu wrapper,etc...)
    - Fixed typos

--- src/lib/components/menuButton.svelte
+++ src/lib/components/menuButton.svelte
@@ -1,14 +1,20 @@
-<script lang=ts>
-    export let hreft: string;
-    export let text: string;
+<script lang="ts">
+	export let href: string;
+	export let text: string;
+	export let icon: string;
+
+	import Icon from '@iconify/svelte';
 </script>
 
-<linkButton>
-    <li class="hover:bg-slate-600 hover:text-slate-100 rounded-md">
-        <a href="{hreft}" class="block w-full p-2">{text}</a>
-    </li>
-</linkButton>
+<li
+	class="rounded-md transition duration-500 ease-in-out hover:scale-105 hover:bg-slate-600 hover:text-slate-100"
+>
+	<a {href} class="block flex w-full items-center gap-2 p-2">
+		<Icon {icon} />
+		<span>{text}</span>
+	</a>
+</li>
 
 <style>
-    @import 'tailwindcss';
+	@import 'tailwindcss';
 </style>
--- src/lib/menuElements.ts
+++ src/lib/menuElements.ts
@@ -1,36 +1,83 @@
 interface INSTRUCTION {
-    name: string;
-    link: string;
-    icon: string|"none"|ImageBitmap;
-    showThis: boolean;
+	name: string;
+	link: string;
+	icon: string;
+	showThis: boolean;
 }
 
-const TOOLS_INSTRUCTIONS: INSTRUCTION[] = [{
-    name: "scadenzario clienti", link: "/scadenzario_clienti", icon: "none",  showThis: true,
-},{
-    name: "scadenzario fornitori", link:"/scadenzario_fornitori", icon: "none", showThis: true,
-},{
-    name: "Analisi marginalità || prodotti", link: "/analisi_margini_prodotti", icon: "none", showThis: true,
-},{
-    name: "Analisi marginalità || clienti", link: "/analisi_margini_clienti", icon: "none", showThis:true,
-},{
-    name: "Esistenza di magazzino", link: "/esistenza", icon: "none", showThis: true,
-},{
-    name: "Genera listino prezzi", link: "/generatore_listino_prezzi", icon: "none", showThis: true,
-}]
+const TOOLS_INSTRUCTIONS: INSTRUCTION[] = [
+	{
+		name: 'Scadenzario clienti',
+		link: '/scadenzario_clienti',
+		icon: 'material-symbols:point-of-sale',
+		showThis: true
+	},
+	{
+		name: 'Scadenzario fornitori',
+		link: '/scadenzario_fornitori',
+		icon: 'material-symbols:delivery-truck-speed-rounded',
+		showThis: true
+	},
+	{
+		name: 'Analisi marginalità || prodotti',
+		link: '/analisi_margini_prodotti',
+		icon: 'material-symbols:balance',
+		showThis: true
+	},
+	{
+		name: 'Analisi marginalità || clienti',
+		link: '/analisi_margini_clienti',
+		icon: 'material-symbols:balance',
+		showThis: true
+	},
+	{
+		name: 'Esistenza di magazzino',
+		link: '/esistenza',
+		icon: 'material-symbols:warehouse-rounded',
+		showThis: true
+	},
+	{
+		name: 'Genera listino prezzi',
+		link: '/generatore_listino_prezzi',
+		icon: 'material-symbols:euro-symbol-rounded',
+		showThis: true
+	}
+];
 
-const HOMEPAGE_INSTRUCTIONS: INSTRUCTION[] = [{
-    name: "Dashboard", link: "/dashboard", icon: "none", showThis: true,
-},{
-    name: "Homepage", link: "/homepage", icon: "none", showThis: false,
-},]
+const HOMEPAGE_INSTRUCTIONS: INSTRUCTION[] = [
+	{
+		name: 'Dashboard',
+		link: '/dashboard',
+		icon: 'material-symbols:home',
+		showThis: true
+	},
+	{
+		name: 'Homepage',
+		link: '/homepage',
+		icon: 'none',
+		showThis: false
+	}
+];
 
-const SETTING_INSTRUCTIONS: INSTRUCTION[] = [{
-    name: "Impostazioni", link: "/impostazioni", icon: "none", showThis: true,
-},{
-    name: "Gestione utenti", link: "/gestione_utenti", icon: "none", showThis: true,
-},{
-    name: "Supporto", link: "/supporto", icon: "none", showThis: true,
-}]
+const SETTING_INSTRUCTIONS: INSTRUCTION[] = [
+	{
+		name: 'Impostazioni',
+		link: '/impostazioni',
+		icon: 'material-symbols:settings',
+		showThis: true
+	},
+	{
+		name: 'Gestione utenti',
+		link: '/gestione_utenti',
+		icon: 'material-symbols:supervised-user-circle-outline',
+		showThis: true
+	},
+	{
+		name: 'Supporto',
+		link: '/supporto',
+		icon: 'material-symbols:help',
+		showThis: true
+	}
+];
 
-export {TOOLS_INSTRUCTIONS, HOMEPAGE_INSTRUCTIONS, SETTING_INSTRUCTIONS}
+export { TOOLS_INSTRUCTIONS, HOMEPAGE_INSTRUCTIONS, SETTING_INSTRUCTIONS };
